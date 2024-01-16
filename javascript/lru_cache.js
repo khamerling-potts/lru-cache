040868981185f21b4294c1d37eb915048297ad6f
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-16 12:57:05 -0500

    done

--- javascript/lru_cache.js
+++ javascript/lru_cache.js
@@ -23,6 +23,7 @@ class DoublyLinkedList {
       node.next = oldhead;
       oldhead.prev = node;
     }
+    this.head.prev = null;
   }
 
   // REMOVE THE TAIL NODE FROM THE LIST
@@ -58,7 +59,10 @@ class DoublyLinkedList {
 
   // MOVE THE GIVEN NODE FROM ITS LOCATION TO THE HEAD
   // OF THE LIST
-  moveNodeToHead(node) {}
+  moveNodeToHead(node) {
+    this.removeNode(node);
+    this.addHead(node);
+  }
 }
 
 class LRUCache {
@@ -66,13 +70,20 @@ class LRUCache {
     this.limit = limit;
     this.size = 0;
     this.hash = {};
-    this.list = new DoublyLinkedList(limit);
+    this.list = new DoublyLinkedList();
   }
 
   // RETRIEVE THE NODE FROM THE CACHE USING THE KEY
   // IF THE NODE IS IN THE CACHE, MOVE IT TO THE HEAD OF THE LIST AND RETURN IT
   // OTHERWISE RETURN -1
-  get(key) {}
+  get(key) {
+    const node = this.hash[key];
+    if (node) {
+      this.list.moveNodeToHead(node);
+      return node;
+    }
+    return -1;
+  }
 
   // ADD THE GIVEN KEY AND VALUE TO THE CACHE
   // IF THE CACHE ALREADY CONTAINS THE KEY, UPDATE ITS VALUE AND MOVE IT TO
@@ -81,25 +92,48 @@ class LRUCache {
   // AT THE HEAD OF THE LIST
   // IF THE CACHE IS FULL, REMOVE THE LEAST RECENTLY USED ITEM BEFORE ADDING
   // THE NEW DATA TO THE CACHE
-  put(key, value) {}
+  put(key, value) {
+    const node = this.get(key);
+    if (node !== -1) {
+      node.data = value;
+      this.list.moveNodeToHead(node);
+    } else {
+      if (this.size === this.limit) {
+        const deletedNode = this.list.removeTail();
+        delete this.hash[deletedNode.key];
+      } else {
+        this.size++;
+      }
+      const newNode = new Node(value, key);
+      this.hash[key] = newNode;
+      this.list.addHead(newNode);
+    }
+  }
 }
 
 if (require.main === module) {
   // add your own tests in here
   const list = new DoublyLinkedList();
   console.log(list.removeTail());
-  list.addHead(new Node((key = "cookies")));
+  list.addHead(new Node("cookie recipe", "cookies"));
   console.log(list);
-  list.addHead(new Node((key = "cake")));
+  const node1 = new Node("cake recipe", "cake");
+  list.addHead(node1);
   console.log(list);
   list.removeTail();
   console.log(list);
-  const node = new Node((key = "brownies"));
-  list.addHead(node);
-  list.addHead(new Node((key = "pie")));
+  const node2 = new Node("brownie recipe", "brownies");
+  list.addHead(node2);
+  list.addHead(new Node("pie recipe", "pie"));
   console.log(list);
-  list.removeNode(node);
+  // list.removeNode(node);
+  // console.log(list);
+  list.moveNodeToHead(node1);
   console.log(list);
+  debugger;
+  const lru = new LRUCache();
+  lru.put("cake", node1);
+  console.log(lru.get("cookies"));
 }
 
 module.exports = {
